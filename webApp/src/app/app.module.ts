170380a88022547953da5aa64794c8bd701717de
Andres Perez <aa.pereza1@uniandes.edu.co>
2023-10-13 02:24:05 -0500

    Reordenados login y registro de candidatos

--- webApp/src/app/app.module.ts
+++ webApp/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
-import { RegCandidatoComponent } from './core/auth/reg-candidato/reg-candidato.component';
+import { RegCandidatoComponent } from './candidates/components/reg-candidato/reg-candidato.component';
 import { LoginCandidatoComponent } from './core/auth/login-candidato/login-candidato.component';
 
 @NgModule({

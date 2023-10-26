c994809cbdbe4ccf05a8cd06d75cf8e3ed15f2db
Andres Perez <aa.pereza1@uniandes.edu.co>
2023-10-25 21:22:00 -0500

    Update environment.development.ts

--- webApp/src/environments/environment.development.ts
+++ webApp/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  HOST: 'http://k8s-proyecto-ingressp-5fc0b96678-541298365.us-east-1.elb.amazonaws.com/'
+  //HOST: 'http://k8s-proyecto-ingressp-5fc0b96678-541298365.us-east-1.elb.amazonaws.com/',
+  HOST: 'http://k8s-proyecto-ingressp-9d93284e4f-309705900.us-east-1.elb.amazonaws.com'
 };

fbc91c5491485b8b90899cc519abf1f3cca91c71
Andres Perez <aa.pereza1@uniandes.edu.co>
2023-10-31 18:12:53 -0500

    Se agregan las pruebas al componente, servicio y clase

--- webApp/src/app/companies/models/empresa.spec.ts
+++ webApp/src/app/companies/models/empresa.spec.ts
@@ -1,7 +1,29 @@
+import { faker } from '@faker-js/faker';
 import { Empresa } from './empresa';
+import { TestBed } from '@angular/core/testing';
 
-describe('Empresa', () => {
-  it('should create an instance', () => {
-    expect(new Empresa()).toBeTruthy();
-  });
+describe('Class Empresa', () => {
+  let empresa: Empresa;
+
+  const password = faker.phone.imei();
+
+  const dataFake = {
+    name: faker.company.name(),
+    email: faker.internet.email(),
+    password: password,
+    confirmPassword: password
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    empresa = new Empresa(
+      dataFake.name,
+      dataFake.email,
+      dataFake.password,
+      dataFake.confirmPassword
+    );
+  })
+  it("Crear instancia de la clase 'Empresa'", () => {
+    expect(empresa).toBeTruthy();
+  })
 });

50edad654397dbc66e8b6855b4a3ad0b2214f517
Andres Perez <aa.pereza1@uniandes.edu.co>
2023-10-31 21:48:32 -0500

    Se agregan las pruebas al servicio

--- webApp/src/app/companies/services/reg-empresa.service.spec.ts
+++ webApp/src/app/companies/services/reg-empresa.service.spec.ts
@@ -50,4 +50,65 @@ describe('Servicio RegEmpresaService', () => {
 
     req.flush(mockResponse);
   })
+
+  it("Método 'getDatosEmpresa', servicio 'RegEmpresaService'", () => {
+    const empresaId: number = 1;
+    const baseUrl = environment.HOST_EMP + 'empresa/' + empresaId;
+    const mockResponse = {
+      "id": 1,
+      "name": faker.company.name(),
+      "organizationType": faker.company.buzzPhrase(),
+      "mail": faker.internet.email(),
+      "docType": 'NIT',
+      "docNumber": faker.phone.number(),
+      "description": faker.lorem.paragraph(5)
+    }
+
+    service.getDatosEmpresa(empresaId).subscribe({
+      next: response => {
+        expect(response).toEqual(mockResponse)
+      }
+    })
+    const req = httpMock.expectOne(baseUrl);
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockResponse);
+  })
+
+  it("Método 'updateDatosEmpresa', servicio 'RegEmpresaService'", () => {
+    const empresaId: number = 1;
+    const pass: string = faker.lorem.word(10);
+    const baseUrl = environment.HOST_EMP + 'empresa/' + empresaId;
+
+    const newEmpresa: Empresa = new Empresa(
+      faker.company.name(),
+      faker.internet.email(),
+      pass,
+      pass,
+      faker.company.buzzPhrase(),
+      'NIT',
+      faker.phone.number(),
+      faker.lorem.paragraph(5),
+      empresaId
+    )
+    const mockResponse = {
+      "id": empresaId,
+      "name": newEmpresa.name,
+      "organizationType": newEmpresa.organizationType,
+      "mail": newEmpresa.mail,
+      "docType": newEmpresa.docType,
+      "docNumber": newEmpresa.docNumber,
+      "description": newEmpresa.description
+    }
+
+    service.updateDatosEmpresa(newEmpresa).subscribe({
+      next: response => {
+        expect(response).toEqual(mockResponse)
+      }
+    })
+    const req = httpMock.expectOne(baseUrl);
+    expect(req.request.method).toBe('PATCH');
+
+    req.flush(mockResponse);
+  })
 });

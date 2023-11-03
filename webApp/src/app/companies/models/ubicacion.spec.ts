3b5c9ab117e957147c6915eb37b7c1679bb5bf06
Andres Perez <aa.pereza1@uniandes.edu.co>
2023-11-03 09:33:44 -0500

    Se agregan pruebas para la adición de ubicaciones

--- webApp/src/app/companies/models/ubicacion.spec.ts
+++ webApp/src/app/companies/models/ubicacion.spec.ts
@@ -1,7 +1,25 @@
+import { faker } from '@faker-js/faker';
 import { Ubicacion } from './ubicacion';
+import { TestBed } from '@angular/core/testing';
 
 describe('Ubicacion', () => {
-  it('should create an instance', () => {
-    expect(new Ubicacion()).toBeTruthy();
-  });
+  let ubicacion: Ubicacion;
+
+  const dataFake = {
+    country: faker.location.country(),
+    city: faker.location.city(),
+    description: faker.lorem.words(5)
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    ubicacion = new Ubicacion(
+      dataFake.country,
+      dataFake.city,
+      dataFake.description
+    );
+  })
+  it("Crear instancia de la clase 'Ubicacion'", () => {
+    expect(ubicacion).toBeTruthy();
+  })
 });

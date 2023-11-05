1f304699d34a022634bb241db5b7c21a0bbada2a
Andres Perez <aa.pereza1@uniandes.edu.co>
2023-11-05 02:45:35 -0500

    Configuración de guardado de Ids en localStorage

--- webApp/src/app/angular-material/angular-material.module.ts
+++ webApp/src/app/angular-material/angular-material.module.ts
@@ -14,6 +14,7 @@ import { MatNativeDateModule } from "@angular/material/core";
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatSelectModule } from "@angular/material/select";
+import { MatMenuModule } from "@angular/material/menu";
 
 @NgModule({
   declarations: [],
@@ -31,7 +32,8 @@ import { MatSelectModule } from "@angular/material/select";
     MatNativeDateModule,
     MatPaginatorModule,
     MatTableModule,
-    MatSelectModule
+    MatSelectModule,
+    MatMenuModule
   ],
   exports: [
     MatButtonModule,
@@ -46,7 +48,8 @@ import { MatSelectModule } from "@angular/material/select";
     MatNativeDateModule,
     MatPaginatorModule,
     MatTableModule,
-    MatSelectModule
+    MatSelectModule,
+    MatMenuModule
   ]
 })
 export class AngularMaterialModule { }

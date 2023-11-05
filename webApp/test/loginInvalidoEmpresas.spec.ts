dab36bd3a9d2514c47770875e95c405d82410965
“ingecar” <j.cardonao@uniandes.edu.co>
2023-11-05 18:42:23 -0500

    Test login candidatos y empresas

---
+++ webApp/test/loginInvalidoEmpresas.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:4200/login');
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await page.getByText('Correo').click();
+  await page.getByLabel('Correo').fill('tecweb@gmail.com');
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await page.getByText('Contraseña', { exact: true }).click();
+  await page.getByLabel('Contraseña', { exact: true }).fill('qwerty');
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await page.getByLabel('Rol').locator('path').click();
+  await page.getByRole('option', { name: 'Empresa' }).click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await page.getByLabel('Rol').locator('svg').click();
+  await page.getByRole('option', { name: 'Candidato' }).click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await page.getByLabel('Rol').locator('path').click();
+  await page.getByRole('option', { name: 'Empresa' }).click();
+  await page.getByLabel('Recordar contraseña').check();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+});
---
+++ webApp/test/loginValidoEmpresas.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:4200/login');
+  await page.getByLabel('Correo').click();
+  await page.getByLabel('Correo').fill('tecnoweb@gmail.com');
+  await page.getByText('Contraseña', { exact: true }).click();
+  await page.getByLabel('Contraseña', { exact: true }).fill('qwerty');
+  await page.getByLabel('Rol').locator('span').click();
+  await page.getByText('Empresa').click();
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await page.locator('button').filter({ hasText: 'logout' }).click();
+});

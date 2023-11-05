cb3e5bb4e6d94c15da579af0b30cc2fb732dda92
Andres Perez <aa.pereza1@uniandes.edu.co>
2023-11-05 16:34:39 -0500

    Configuración de entorno aws

--- webApp/src/environments/environment.development.ts
+++ webApp/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  //HOST_CAND: 'http://k8s-proyecto-ingressp-9d93284e4f-309705900.us-east-1.elb.amazonaws.com/',
-  //HOST_EMP: 'http://k8s-proyecto-ingressp-9d93284e4f-309705900.us-east-1.elb.amazonaws.com/',
-  HOST_CAND: 'http://localhost:3001/',
-  HOST_EMP: 'http://localhost:3003/'
+  HOST_CAND: 'http://k8s-proyecto-ingressp-8502bda72b-1275865132.us-east-1.elb.amazonaws.com/',
+  HOST_EMP: 'http://k8s-proyecto-ingressp-8502bda72b-1275865132.us-east-1.elb.amazonaws.com/',
+  //HOST_CAND: 'http://localhost:3001/',
+  //HOST_EMP: 'http://localhost:3003/'
 }
 

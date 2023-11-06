e4284e811d018737edde6728855a52e21e37d83e
“ingecar” <j.cardonao@uniandes.edu.co>
2023-11-05 22:15:13 -0500

    Test vertical empresa

---
+++ webApp/test/editarInformacionEmpresa.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByLabel('Correo').fill('tecnoweb@gmail.com');
+  await page.getByLabel('Correo').click();
+  await page.getByLabel('Correo').press('Tab');
+  await page.getByLabel('Contraseña', { exact: true }).fill('qwerty');
+  await page.getByLabel('Rol').locator('span').click();
+  await page.getByText('Empresa').click();
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await page.getByRole('button', { name: 'Editar' }).click();
+  await page.getByLabel('Número de documento').click();
+  await page.getByLabel('Número de documento').fill('');
+  await page.getByLabel('Número de documento').fill('12345-0');
+  await page.getByRole('button', { name: 'Guardar' }).click();
+  await page.locator('button').filter({ hasText: 'logout' }).click();
+});

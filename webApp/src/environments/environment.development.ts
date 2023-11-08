9b7cc221aabc6177d0319152853fc1f4c8092f55
Andres Perez <aa.pereza1@uniandes.edu.co>
2023-11-08 09:22:54 -0500

    Actualizado el host del back AWS

--- webApp/src/environments/environment.development.ts
+++ webApp/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  HOST_CAND: 'http://k8s-proyecto-ingressp-8502bda72b-1275865132.us-east-1.elb.amazonaws.com/',
-  HOST_EMP: 'http://k8s-proyecto-ingressp-8502bda72b-1275865132.us-east-1.elb.amazonaws.com/',
+  HOST_CAND: 'http://k8s-proyecto-ingressp-eb59205740-1747100398.us-east-1.elb.amazonaws.com/',
+  HOST_EMP: 'http://k8s-proyecto-ingressp-eb59205740-1747100398.us-east-1.elb.amazonaws.com/',
   //HOST_CAND: 'http://localhost:3001/',
   //HOST_EMP: 'http://localhost:3003/'
 }

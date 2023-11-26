c95c3c77afce09ed3a697f5288609329f2bf1c63
PereAnd <aa.pereza1@uniandes.edu.co>
2023-11-26 15:55:16 -0500

    Configuración de nuevos links en el environment

--- webApp/src/environments/environment.development.ts
+++ webApp/src/environments/environment.development.ts
@@ -1,10 +1,11 @@
 export const environment = {
   // HOST_CAND: 'http://k8s-proyecto-ingressp-eb59205740-1747100398.us-east-1.elb.amazonaws.com/',
   // HOST_EMP: 'http://k8s-proyecto-ingressp-eb59205740-1747100398.us-east-1.elb.amazonaws.com/',
-  HOST_CAND: 'http://localhost:3001/',
-  HOST_EMP: 'http://localhost:3002/',
-  HOST_PERF: 'http://localhost:3003/',
-  HOST_ENTR: 'http://localhost:3004/',
-  HOST_BUSQ: 'http://localhost:3005/',
+  HOST_CAND: 'http://k8s-proyecto-ingressp-4cd6368b1b-1854900434.us-east-1.elb.amazonaws.com',
+  HOST_EMP: 'http://k8s-proyecto-ingressp-4cd6368b1b-1854900434.us-east-1.elb.amazonaws.com',
+  HOST_PERF: 'http://k8s-proyecto-ingressp-4cd6368b1b-1854900434.us-east-1.elb.amazonaws.com',
+  HOST_ENTR: 'http://k8s-proyecto-ingressp-4cd6368b1b-1854900434.us-east-1.elb.amazonaws.com',
+  HOST_BUSQ: 'http://k8s-proyecto-ingressp-4cd6368b1b-1854900434.us-east-1.elb.amazonaws.com',
+  HOST_ANGULAR: 'http://bucket-abcjobs-angular.s3-website-us-east-1.amazonaws.com/',
 }
 

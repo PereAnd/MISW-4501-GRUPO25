3f1ef4e9168af7dade5e2f294b8f192a79793a44
PereAnd <aa.pereza1@uniandes.edu.co>
2023-11-26 19:23:50 -0500

    Agregada la aplicación desde el buscador

--- webApp/src/app/companies/components/dashboard-emp/busqueda-cand/detail-cand/detail-cand.component.ts
+++ webApp/src/app/companies/components/dashboard-emp/busqueda-cand/detail-cand/detail-cand.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Candidato } from 'src/app/candidates/models/candidato';
 import { RegCandidatoService } from 'src/app/candidates/services/reg-candidato.service';
+import { CreateApplComponent } from '../create-appl/create-appl.component';
+import { PerfilesService } from 'src/app/companies/services/perfiles.service';
 
 @Component({
   selector: 'app-detail-cand',
@@ -9,12 +12,32 @@ import { RegCandidatoService } from 'src/app/candidates/services/reg-candidato.s
 })
 export class DetailCandComponent {
   candidatoDetail: Candidato;
+  empresaId: number;
+  aplicaciones: number[] = [];
 
   constructor(
-    private candidatosService: RegCandidatoService
-  ){}
+    private candidatosService: RegCandidatoService,
+    private perfilesService: PerfilesService,
+    public dialog: MatDialog
+  ){
+    this.empresaId = +localStorage.getItem('empresaId')!;
+  }
 
   ngOnInit(): void {
     this.candidatoDetail = this.candidatosService.getCandidateForDetail()
+    this.perfilesService.listAplicacionesEmpresa(this.empresaId).subscribe({
+      next: listAplicaciones => {
+        listAplicaciones.forEach(aplicacion => {
+          this.aplicaciones.push(aplicacion.candidatoId)
+        })
+      }
+    })
+  }
+  createAplicacion(candidatoId: number){
+    // this.candidatosService.setCandidateForDetail(candidate);
+    const dialogRef = this.dialog.open(CreateApplComponent, { width: '600' });
+    dialogRef.afterClosed().subscribe(result => {
+      this.ngOnInit();
+    });
   }
 }
